9a1d503b5f2a3e43e8077f4d3ebadb96503aa65f
gregscardoso <gregorycardosogravinez@gmail.com>
2026-07-07 21:29:36 -0300

    style: adiciona tema visual da home (topbar, cards de atalhos e modulos)

--- integracoes_customizadas/hooks.py
+++ integracoes_customizadas/hooks.py
@@ -114,8 +114,8 @@ override_doctype_class = {
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/integracoes_customizadas/css/integracoes_customizadas.css"
-# app_include_js = "/assets/integracoes_customizadas/js/integracoes_customizadas.js"
+app_include_css = "/assets/integracoes_customizadas/css/desk_theme.css"
+# app_include_js = "/assets/integracoes_customizadas/js/desk_theme.js"  # desativado: so servia pro experimento de topbar teal, revertido
 
 # include js, css files in header of web template
 # web_include_css = "/assets/integracoes_customizadas/css/integracoes_customizadas.css"
